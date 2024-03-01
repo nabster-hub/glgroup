5bb2a65bcfd97050428360f88f091cad25b6f7e8
Naimanshalov Ilias <mail@nabster.name>
2024-03-02 02:01:42 +0600

    add blocks create and adaptive to 768px

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "@/app/components/Hero/Hero";
 import AboutUs from "@/app/components/AboutUs/AboutUs";
+import Blocks from "@/app/components/Blocks/Blocks";
 
 export default function page() {
     return (
        <>
-            <Hero />
+           <Hero />
            <AboutUs />
+           <Blocks />
        </>
     );
 }
