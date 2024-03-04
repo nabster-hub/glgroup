d71e11503cc4448ea662e7bd34c35814052d7320
Naimanshalov Ilias <mail@nabster.name>
2024-03-04 21:06:35 +0600

    add admin panel global settings

--- app/components/header/header.jsx
+++ app/components/header/header.jsx
@@ -11,41 +11,36 @@ import clsx from "clsx";
 import MobileMenu from "@/app/components/MobileMenu/MobileMenu";
 
 const Header = ({links}) => {
-    console.log(links[0])
     const itemLinks = links?.links;
-    console.log(itemLinks);
+    const socials = links?.socials;
+    const mail = links?.label;
+    const mailIcon = links?.icon;
+    const whatsappNum = links?.numberWhatsapp;
+    const whatsappIcon = links?.imageWhatsapp;
     return (
         <header className={clsx('py-5 mb-6 text-white', styles.header)}>
 
             <div className={'container flex justify-between h-full'}>
                 <div className={'flex gap-10 font-gilroy text-base font-normal'}>
-                    {links?.links && links.links.map((e, _uid)=>(
-                         <Link href={e.link} className={'hover:text-yellow-active'}>{e.label}</Link>
+                    {itemLinks && itemLinks.map((e, _uid)=>(
+                         <Link href={e.link.linktype === "story" ? "/"+e.link.cached_url : e.link.cached_url} key={_uid} className={'hover:text-yellow-active'}>{e.label}</Link>
                     ))}
-
-                    <Link href={'#'} className={'hover:text-yellow-active'}>Контакты</Link>
-                    <Link href={'#'} className={'hover:text-yellow-active'}>Услуги</Link>
-                    <Link href={'#'} className={'hover:text-yellow-active'}>Блог</Link>
                 </div>
                 <div className={'flex gap-10'}>
                     <div className={'flex items-center gap-4'}>
-                        <Link href={'#'}>
-                            <Image src={telegram} alt={'telegram'}></Image>
-                        </Link>
-                        <Link href={'#'}>
-                            <Image src={vk} alt={'vk'}></Image>
-                        </Link>
-                        <Link href={'#'}>
-                            <Image src={fb} alt={'facebook'}></Image>
-                        </Link>
+                        {socials && socials.map((e, _uid)=>(
+                            <Link href={e.link.linktype === "story" ? "/"+e.link.cached_url : e.link.cached_url } key={_uid}>
+                                <Image src={e.image.filename} width={'20'} height={'20'} alt={e.image.alt}></Image>
+                            </Link>
+                        ))}
                     </div>
                     <div className={'flex gap-3 items-center hover:text-yellow-active '}>
-                        <Image src={letter} alt={'email'}/>
-                        <span className={'font-gilroy font-bold text-sm'}>info@gl-group.consulting </span>
+                        <Image src={mailIcon.filename} width={'24'} height={'24'} alt={mailIcon.alt}/>
+                        <span className={'font-gilroy font-bold text-sm'}>{mail}</span>
                     </div>
                     <div className="flex gap-3 hover:text-yellow-active">
-                        <Image src={whatsapp} alt={'WhatsApp'}/>
-                        <span className={'font-gilroy font-bold text-xl'}>+62 813 987 65 54</span>
+                        <Image src={whatsappIcon.filename} width={'21'} height={'21'} alt={whatsappIcon.alt}/>
+                        <span className={'font-gilroy font-bold text-xl'}>{whatsappNum}</span>
                     </div>
                     <div className="flex gap-3 font-gilroy font-bold text-sm items-center">
                         <Link href={'#'} className={'text-yellow-active hover:text-yellow-active'}>
--- app/components/menu/menu.jsx
+++ app/components/menu/menu.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import clsx from "clsx";
 
-const Menu = () => {
+const Menu = ({menu}) => {
     return (
         <div className={'container flex justify-between text-white'}>
             <div className={'flex gap-2.5 items-center'}>
@@ -12,26 +12,16 @@ const Menu = () => {
                            width={59}
                            height={59}
                     />
-                    <span className={'max-w-[136px] font-extrabold text-xl uppercase'}>Good Luck Group</span>
+                    <span className={'max-w-[136px] font-extrabold text-xl uppercase'}>{menu.siteName}</span>
                 </Link>
-                <span className={'text-sm lowercase max-w-[139px] leading-4'}>международное консалтинговое агентство</span>
+                <span className={'text-sm lowercase max-w-[139px] leading-4'}>{menu.siteDescription}</span>
             </div>
-            <div className={'flex gap-16 py-2.5 items-center font-gilroy font-bold text-lg'}>
-                <Link href={'#'} className={'hover:text-yellow-active'}>
-                    Главная
-                </Link>
-                <Link href={'#'} className={'hover:text-yellow-active'}>
-                    О нас
-                </Link>
-                <Link href={'#'} className={'hover:text-yellow-active'}>
-                    Преимущества
-                </Link>
-                <Link href={'#'} className={'hover:text-yellow-active'}>
-                    Контакты
-                </Link>
-                <Link href={'#'} className={clsx('border-yellow-active border rounded-3xl text-yellow-active px-3 py-2.5 hover:bg-yellow-active hover:text-black')}>
-                    Обратный звонок
-                </Link>
+            <div className={'flex gap-10 py-2.5 items-center font-gilroy font-bold text-lg'}>
+                {menu?.links && menu.links.map((e, _uid)=>(
+                            <Link href={e.link.linktype === "story" ? "/"+e.link.cached_url : e.link.cached_url} key={_uid} className={clsx(!e.contact && 'hover:text-yellow-active', e.contact && 'border-yellow-active text-yellow-active border rounded-3xl  px-3 py-2.5 hover:bg-yellow-active hover:text-black')}>
+                                {e.label}
+                            </Link>
+                   ))}
             </div>
         </div>
 
