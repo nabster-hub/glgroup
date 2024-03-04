2cd1672ab4c86f6de397904ae1e94a05f2db7691
Naimanshalov Ilias <mail@nabster.name>
2024-03-04 18:00:17 +0500

    try to get links

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,13 +2,22 @@ import Image from "next/image";
 import Hero from "@/app/components/Hero/Hero";
 import AboutUs from "@/app/components/AboutUs/AboutUs";
 import Blocks from "@/app/components/Blocks/Blocks";
+import {getStoryblokApi} from "@storyblok/react";
 
-export default function page() {
+export default async function page() {
+    const {data} = await fetchData();
     return (
        <>
-           <Hero />
+           <Hero topmenu={data.story?.content?.linkMenu} />
            <AboutUs />
            <Blocks />
        </>
     );
+}
+
+export async function fetchData() {
+    let sbParams = { version: "draft" };
+
+    const storyblokApi = getStoryblokApi();
+    return storyblokApi.get(`cdn/stories/global`, sbParams, {cache: "no-store"});
 }
