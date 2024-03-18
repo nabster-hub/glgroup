66395ef1cad59d4a6e12701bd14fa16a8c10d3d9
Naimanshalov Ilias <mail@nabster.name>
2024-03-18 16:40:00 +0500

    add slider partners

---
+++ app/components/OurPartners/OurPartners.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './OurPartners.module.scss';
+import Slider from './Slider';
+
+
+const OurPartners = ({blok}) => {
+    //console.log(blok)
+    return (
+        <section>
+            <div className="container py-32">
+                <div className={styles.blockTitle}>
+                    <span className={styles.subTitle}>{blok.subTitle}</span>
+                    <h2 className={'h2'}>{blok.title}</h2>
+                </div>
+                <div className={styles.slider}>
+                    {blok?.sliders && (
+                        <Slider items={blok?.sliders} />
+                    )}
+                </div>
+
+            </div>
+        </section>
+    );
+};
+
+
+export default OurPartners;
