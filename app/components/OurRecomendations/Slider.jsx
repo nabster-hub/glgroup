e9afd4edb4354021c200951237c57071b71a5819
Naimanshalov Ilias <mail@nabster.name>
2024-03-19 02:18:15 +0500

    edit fonts, edit slider, create ourRecomendations

--- app/components/OurRecomendations/Slider.jsx
+++ app/components/OurRecomendations/Slider.jsx
@@ -1,7 +1,7 @@
 "use client";
 import {Swiper, SwiperSlide, useSwiper} from 'swiper/react';
 import { Pagination, Navigation } from 'swiper/modules';
-import styles from './OurPartners.module.scss'
+import styles from './OurRecomendations.module.scss'
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
@@ -11,14 +11,16 @@ import Link from "next/link";
 import Image from "next/image";
 
 const Slider = ({items}) => {
+    //const paginationRef = useRef(null);
     const arrowPrev = useRef(null);
     const arrowNext = useRef(null);
-    //console.log(data)
+
+    console.log(items)
     return (
         <div className={styles.sliders}>
             <Swiper
                 slidesPerView={3}
-                spaceBetween={10}
+                spaceBetween={26}
                 modules={[Navigation]}
                 navigation={{
                     prevEl: arrowPrev.current,
@@ -28,31 +30,21 @@ const Slider = ({items}) => {
                     swiper.params.navigation.prevEl = arrowPrev.current;
                     swiper.params.navigation.nextEl = arrowNext.current;
                 }}
-
-                className="partners"
+                className="recomends"
             >
-                    {items.map((e, _uid)=>(
-                        <SwiperSlide key={_uid}>
-                            <div className={styles.slideContainer}>
-                                <Image src={e.image.filename}
-                                       fill
-                                       quality={80}
-
-                                       sizes="(max-width: 768px) 50vw,
-                             (max-width: 1200px) 25vw,
-                             25vw"
-                                       style={{
-                                           objectFit: "contain",
-                                       }}
-                                       alt={e.image.alt}
-                                />
-                            </div>
-
-
-                        </SwiperSlide>
-                    ))}
-
                 <div className="swiper-button-prev" ref={arrowPrev}></div>
+                {items.map((e, _uid) => (
+                    <SwiperSlide key={_uid}>
+                        <div className={styles.slideContainer}>
+                            <span className={styles.quotes}>“</span>
+                            <h3>{e.name}</h3>
+                            <span className={styles.job}>{e.jobTitle}</span>
+                            <p>
+                                {e.text}
+                            </p>
+                        </div>
+                    </SwiperSlide>
+                ))}
                 <div className="swiper-button-next" ref={arrowNext}></div>
 
             </Swiper>
