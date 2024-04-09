9fd3387ced189b45629e0c97881a2bef71660cd5
Naimanshalov Ilias <mail@nabster.name>
2024-04-10 00:19:13 +0500

    add pages, some fixes

--- app/components/menu/menu.jsx
+++ app/components/menu/menu.jsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 import Image from "next/image";
 import clsx from "clsx";
 
-const Menu = ({menu}) => {
+const Menu = ({menu, type}) => {
     return (
-        <div className={'container hidden lg:flex justify-between text-white'}>
+        <div className={clsx('container hidden lg:flex justify-between', type ? 'text-black' : 'text-white')}>
             <div className={'flex gap-2 xl:gap-2.5 items-center'}>
                 <Link href={'#'} className="flex items-center lg:gap-2 xl:gap-4">
                     <Image src={'/img/logo.svg'} alt={"Logo"}
@@ -19,7 +19,7 @@ const Menu = ({menu}) => {
             </div>
             <div className={'flex lg:gap-5 xl:gap-10 py-2.5 items-center font-gilroy font-bold text-base xl:text-lg'}>
                 {menu?.links && menu.links.map((e, _uid)=>(
-                            <Link href={e.link.linktype === "story" ? "/"+e.link.cached_url : e.link.cached_url} key={_uid} className={clsx(!e.contact && 'hover:text-yellow-active', e.contact && 'border-yellow-active text-yellow-active border rounded-3xl lg:px-2.5 lg:py-2 xl:px-3 xl:py-2.5 hover:bg-yellow-active hover:text-black')}>
+                            <Link href={e.link.linktype === "story" ? "/"+e.link.cached_url : e.link.cached_url} key={_uid} className={clsx(!e.contact && type ? 'hover:text-green-active' :'hover:text-yellow-active', e.contact && !type && 'border-yellow-active text-yellow-active border rounded-3xl lg:px-2.5 lg:py-2 xl:px-3 xl:py-2.5 hover:bg-yellow-active hover:text-black', e.contact && type && 'border-green-active text-green-active border rounded-3xl lg:px-2.5 lg:py-2 xl:px-3 xl:py-2.5 hover:bg-green-active hover:text-black')}>
                                 {e.label}
                             </Link>
                    ))}
