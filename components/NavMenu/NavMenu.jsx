3abc9be33f259848311c12bca00108e8d213efa1
Naimanshalov Ilias <mail@nabster.name>
2024-04-11 18:54:57 +0500

    fixed menu when scroll

--- components/NavMenu/NavMenu.jsx
+++ components/NavMenu/NavMenu.jsx
@@ -5,10 +5,14 @@ import Header from "@/components/header/header";
 import Menu from "@/components/menu/menu";
 import MobileMenu from "@/components/MobileMenu/MobileMenu";
 import {usePathname} from "next/navigation";
+import useScrollOffset from "@/lib/offset";
+import styles from "./NavMenu.module.scss"
 
 export default function NavMenu ({headMenu, menu}) {
     const pathname = usePathname();
     const [white, setWhite] = useState(false);
+    const [fixed, setFixed] = useState(false);
+    const offset = useScrollOffset();
 
     const paths = [
         'rules',
@@ -21,12 +25,20 @@ export default function NavMenu ({headMenu, menu}) {
             return false;
         }
     }
-
+    const fix = ()=>{
+        if(offset > 50){
+            return true;
+        }else {
+            return false;
+        }
+    }
     useEffect(() => {
         setWhite(checkType(pathname))
-    }, [pathname]);
+        setFixed(fix)
+    }, [pathname, offset]);
+
     return (
-        <header className={clsx(!white ? 'absolute top-0  w-full z-10' : "w-full mb-5 lg:mb-12")}>
+        <header className={clsx(!white ? `w-full z-10 ${styles.blackMenu}` : `w-full mb-5 lg:mb-12 ${styles.whiteMenu}`, fixed ? styles.fixed : '')}>
             <Header links={headMenu} type={white}/>
             <Menu menu={menu} type={white}/>
             <MobileMenu menu={headMenu} link={menu} type={white}/>
