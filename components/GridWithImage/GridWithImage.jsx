51694f2d0ba1ee9f8fa43683b94a42c08bae15d8
Naimanshalov Ilias <mail@nabster.name>
2024-04-26 01:55:57 +0500

    services blok

---
+++ components/GridWithImage/GridWithImage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './GridWithImage.module.scss'
+import {StoryblokComponent} from "@storyblok/react/rsc";
+const GridWithImage = ({blok}) => {
+    return (
+        <section className={styles.gridWithImage}>
+            <div className="container">
+                <div className={styles.grid}>
+                    {blok?.blocks && blok.blocks.map((e, _uid)=>(
+                        <StoryblokComponent blok={e} key={_uid}/>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+};
+
+
+export default GridWithImage;
