c921d27ff46da433b53b4b87800fefe9d151f0a9
Naimanshalov Ilias <mail@nabster.name>
2024-04-28 23:50:09 +0500

    create author page and some fixes

--- components/Breadcrumbs/Breadcrumbs.jsx
+++ components/Breadcrumbs/Breadcrumbs.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import styles from './Breadcrumbs.module.scss'
 import Link from "next/link";
 import {console} from "next/dist/compiled/@edge-runtime/primitives";
+import clsx from "clsx";
 
-const Breadcrumbs = ({links}) => {
+const Breadcrumbs = ({links, author}) => {
    const lastSearch = (arr) =>{
        for(let i =0; i<arr.length; i++){
             if(i === arr.length-1){
@@ -15,7 +16,7 @@ const Breadcrumbs = ({links}) => {
     return (
         <section >
             <div className="container">
-                <div className={styles.container}>
+                <div className={clsx(styles.container, author && styles.author)}>
                     {links.map((e, _uid)=>(
                         <div className={styles.block} key={_uid}>
                             <Link className={styles.link} href={e.link.linktype === "story" ? "/" + e.link.cached_url : e.link.cached_url}>
