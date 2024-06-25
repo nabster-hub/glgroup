b7063d523c6d8b48ac94692be44cf6770cae548c
Naimanshalov Ilias <mail@nabster.name>
2024-06-25 14:51:44 +0500

    change storyblock init

--- app/[locale]/services/[slug]/page.jsx
+++ app/[locale]/services/[slug]/page.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {fetchData} from "@/lib/api";
 import {StoryblokComponent} from "@storyblok/react/rsc";
 import {unstable_setRequestLocale} from "next-intl/server";
+import {notFound} from "next/navigation";
 
 export const revalidate = 3600;
 
 export async function generateMetadata({params, params:{locale}},parent){
     unstable_setRequestLocale(locale);
     const {data} = await fetchData(`/services/${params.slug}`, {version: 'draft', language: locale});
+
     return{
         title: data.story.content.metaTitle,
         description: data.story.content.metaDescription,
@@ -17,6 +19,9 @@ export async function generateMetadata({params, params:{locale}},parent){
 export default async function Page({params, params:{locale}}) {
     unstable_setRequestLocale(locale);
     const {data} = await fetchData(`/services/${params.slug}`, {version: 'draft', language: locale});
+    if(!data){
+        notFound();
+    }
     return (
         <StoryblokComponent blok={data?.story.content} />
     );
