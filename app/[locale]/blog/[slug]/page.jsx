af388075f1ae868a6369aa984aae7c68991908eb
Naimanshalov Ilias <mail@nabster.name>
2024-06-29 01:40:56 +0500

    fixed 500 error on 404

--- app/[locale]/blog/[slug]/page.jsx
+++ app/[locale]/blog/[slug]/page.jsx
@@ -3,18 +3,32 @@ import {fetchData} from "@/lib/api";
 import Breadcrumbs from "@/components/Breadcrumbs/Breadcrumbs";
 import Post from "@/components/Post/Post";
 import {unstable_setRequestLocale} from "next-intl/server";
+import {notFound} from "next/navigation";
 export const revalidate = 3600;
 export async function generateMetadata({params, params: {locale}}, parent){
     unstable_setRequestLocale(locale);
-    const {data} = await fetchData("blog/"+params.slug, {version: 'draft', language: locale});
-    return{
-        title: data.story.content.metaTitle,
-        description: data.story.content.metaDescription,
+    const res = await fetchData("blog/"+params.slug, {version: 'draft', language: locale});
+    if(!res){
+        return {
+            title: "Page not found",
+            description: "Page not found",
+        }
+    }else{
+        const {data} = res
+        return{
+            title: data.story.content.metaTitle,
+            description: data.story.content.metaDescription,
+        }
     }
 }
 export default async function Page({params, params: {locale}}){
     unstable_setRequestLocale(locale);
-    const {data} = await fetchData("blog/"+params.slug, {version: 'draft', language: locale});
+    const res = await fetchData("blog/"+params.slug, {version: 'draft', language: locale});
+
+    if(!res){
+        notFound()
+    }
+    const {data} = res
 
     return (
         <>
--- app/[locale]/services/[slug]/page.jsx
+++ app/[locale]/services/[slug]/page.jsx
@@ -8,20 +8,29 @@ export const revalidate = 3600;
 
 export async function generateMetadata({params, params:{locale}},parent){
     unstable_setRequestLocale(locale);
-    const {data} = await fetchData(`/services/${params.slug}`, {version: 'draft', language: locale});
+    const res = await fetchData(`/services/${params.slug}`, {version: 'draft', language: locale});
 
-    return{
-        title: data.story.content.metaTitle,
-        description: data.story.content.metaDescription,
+    if(!res){
+        return {
+            title: "Page not found",
+            description: "Page not found",
+        }
+    }else{
+        const {data} = res
+        return{
+            title: data.story.content.metaTitle,
+            description: data.story.content.metaDescription,
+        }
     }
 }
 
 export default async function Page({params, params:{locale}}) {
     unstable_setRequestLocale(locale);
-    const {data} = await fetchData(`/services/${params.slug}`, {version: 'draft', language: locale});
-    if(!data){
+    const res = await fetchData(`/services/${params.slug}`, {version: 'draft', language: locale});
+    if(!res){
         notFound();
     }
+    const {data} = res
     return (
         <StoryblokComponent blok={data?.story.content} />
     );
