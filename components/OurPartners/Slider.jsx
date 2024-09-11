9c20d7a2d10238959ef4b78fa97f2f4e9e3dd71b
Naimanshalov Ilias <mail@nabster.name>
2024-09-11 10:17:53 +0500

    fixed slider loop bug on ipads

--- components/OurPartners/Slider.jsx
+++ components/OurPartners/Slider.jsx
@@ -30,6 +30,14 @@ const Slider = ({items}) => {
                         slidesPerView: 2,
                         spaceBetween: 10
                     },
+                    520: {
+                      slidesPerView: 3,
+                      spaceBetween: 10
+                    },
+                    768:{
+                        slidesPerView: 4,
+                        spaceBetween: 10
+                    },
                     1024: {
                         slidesPerView:3,
                         spaceBetween: 10
