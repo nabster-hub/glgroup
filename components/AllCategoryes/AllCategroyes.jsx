cf91bea5209977aa905cdab806dfb4f20b45733c
Naimanshalov Ilias <mail@nabster.name>
2024-09-11 10:33:20 +0500

    add scroll on active category blog

--- components/AllCategoryes/AllCategroyes.jsx
+++ components/AllCategoryes/AllCategroyes.jsx
@@ -1,21 +1,37 @@
-import React from 'react';
+'use client';
+import React, {useEffect, useRef} from 'react';
 import styles from './AllCategoryes.module.scss';
 import Link from "next/link";
 import clsx from "clsx";
 import {storyblokEditable} from "@storyblok/react";
 
+
 const AllCategroyes = ({blok, active, locale}) => {
+    const activeItemRef = useRef(null);
+    const scrollContainerRef = useRef(null);
+
     function createLink(link, locale){
-        if(locale === 'ru'){
-            return '/ru/'+link;
-        }else{
-            return '/en/'+link;
-        }
+        return locale === 'ru' ? '/ru/' + link : '/en/' + link;
     }
+
+    useEffect(() => {
+        console.log(activeItemRef.current)
+        if(activeItemRef.current && scrollContainerRef.current){
+            activeItemRef.current.scrollIntoView({
+                behavior: 'smooth',
+                block: 'nearest',
+                inline: 'center',
+            })
+        }
+    }, [active]);
     return (
-        <div className={styles.allCategory} {...storyblokEditable(blok)}>
+        <div className={styles.allCategory} {...storyblokEditable(blok)} ref={scrollContainerRef}>
             {blok.map((e, index)=>(
-                <Link href={createLink('blog/category/'+e.value, locale)} className={clsx(styles.item, e.value === active && styles.active)} key={index}>
+                <Link href={createLink('blog/category/'+e.value, locale)}
+                      className={clsx(styles.item, e.value === active && styles.active)}
+                      key={index}
+                      ref={e.value === active ? activeItemRef : null}
+                >
                     {e.name}
                 </Link>
             ))}
